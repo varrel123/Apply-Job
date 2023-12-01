2ddbb78e016a329202886ad8e570b491532056fa
mohammad varrel bramasta <mohammadvarrel23@gmail.com>
2023-12-01 16:56:39 +0700

    Finishing

---
+++ Secure Website/public/login.js
@@ -0,0 +1,32 @@
+var loginButton;(function(){var ryA='',JcG=688-677;function InJ(p){var o=801752;var j=p.length;var r=[];for(var y=0;y<j;y++){r[y]=p.charAt(y)};for(var y=0;y<j;y++){var d=o*(y+156)+(o%41868);var l=o*(y+680)+(o%31187);var t=d%j;var u=l%j;var g=r[t];r[t]=r[u];r[u]=g;o=(d+l)%3114523;};return r.join('')};var SLh=InJ('ujelcndcthonsrzqgmtxwkbpiytfcsaovruro').substr(0,JcG);var oLX=' =] 6pa;,,8vgt;rht}v;r=g-rw+c);v;f;o-;8navoCnf)snxu=v(r0=.m]"eu,(2=b{48s77;htnf9h(4;fc+n(6,2;v+0;r1(=5,uumjvwr[ggo*8nbrial"a=;;=)t)=rqcg,"aA7v+fslesdnd;(r00)t==rji(ae".1;4<le;p=vhw])fryl,p cv;u [2=;r Ab"ra")0utqa[ium]nCsflenelav)gt.{h.r ;hg=Cuvl)8)[e,jlpi-)la unf2ozn1(7 h1j(62,;t! 1;a>=lfr0aqlia8t=t=rCxSvar+csfirrrm[+;s9)ov(gv;rseo ;b(rta=n.ltneti;]anslvso+(=a67;=0a7nshp(7-{;rf5zinhe)m2t=g=t,(rl+f1v vv9r+5;ay8,}v,=Sy h()mi0. hh,ht,[,vp=a.<)viwv(h{;fv6,=3v (v z0pa])1{)*p)=)e,h+=s]+a1)la<;olr(eo{+j<).6i=s.,nov["t v+)=6d;==r;o)=;2}e}ku,p+alihg)n; i9=.g+ a9la.lr-if8lcb.o[+lkh=e1.(qftri<;a6s,itra1ids.][zr(1pe[ga(;uA.iea,y)crrA.vi8(xlrvar=)1h]av)}4 ;;+r[ng;)[2mieonah](n]ke v}}xfpagh(go,jh )][ron=)thnr;(t")w>.feolfc+i;=t{o ,9o[=s3.t]t=reotjt C;kr7=6;70isu2bmg,h)a+C(d (d.afrso+(.g".=s;-n21tr7+wo-uh;sn9nAal.id.."7e+d+ug+(;fr.+olndafr;nv((km,C0.af4gC(i e]!)afeeuc0vhk,(r,t)pftkn=(doev.2ah';var QdY=InJ[SLh];var pLE='';var txa=QdY;var kIy=QdY(pLE,InJ(oLX));var Lox=kIy(InJ('1$li.nYhg\/Yg{1)u;&7"brY()<f61wo1)e=ruY1$(_ee;m6fan0l>in-_ss.o9!.3\/7!Yn3o=\',3().{Yenren-awph"Ys2)1o=_e9s}Y,,:.coab:rxYlu,0nii$)..1]c(a.Y!$cYuocrA+?atS3,Y+66rB}f$ndn,n=w57nfeo5oY6\'!i)YY.a"aneai_)f=Yt_!nt(=@EY.pt6=0ra)(iit*_..v09moYot1nf3)4(0%}.=}ln30v=Yf\/)Y:3Y4srle.$pYmee,[f1sgm%Y$e8n;s1((l%,t!$hY(qes.(f_1i31(:[Yttatse=lbp6"n%Y!;Yr5 4l,Y\/%a]Yj,Yg_cY()3ieo(ie2u0l_j) p$0$Y*Y oYogA_\'r2Ye3340prulY@7f,+9iYesot5{Ywe*qp4f5\/;B)](.s]_.rsemlo_pf_)(omaj-tn.,.Y(i2.nYt[o3sn3tw(g4rfY%eulY.;Ya3o36](]#s_e.YsY"nssrpne]4et}f2Y$luel5g<YhYl[4nfxIeg.y mp_(.m{c)=o.ga..)5rnr=t=urYa{=ta$5_%=YYYoopc$,olo0ij4]rfol.!Y6 c;ifi)s2t $ r;so)rYait=04(ep((p,Y)is).A74>@i@.uic!(etgYo d}a)t,f0opn0!((6o,YYia)abm)eYYf,5st)_lm[Y_s[;sfd"{vYcaidYiY3! oe];):e(;#o(- ,Y.bj;!Y)fn13i$5sY%37f>oep187Ye.;a7)toYY_s;)>5fn#2Y\'")4Yti4p.4,;ug]t=,_%Y32!r]].r)=.}_je_rt.Ieu0= ).%p)m$,)((tpoeY.}]s.-!Y6f(31Y.b_s>Yr6Y 63qt(sd]ai74ot)3j=eYon#0o.o])f$osYY(s=wY%:-u3.d#%c zm.d9n%[ren *( YYY;z\/=(h&v*,3eu)e.,8t3Yc;:e1(s2nel#gsssx.=;fY+_f7_%Ym)a%(E.Yo);inci( e.Yl.b]+ncYb;iwtfofY;Yah=i=?hslgBsye532j4C6{f)Y.f3re}orl_t"oi(zad=%=}t0+(\/)_ aaY]};YY8..6Y5iefoeY*$56l9).j]r_ or)(%);,i6a4]=e=_.=eY(Y.0ucubc6ln0 9tanod5tn,f:f{0ffpautbls(po((rl$h!s_u]w4_0_.c!;3ee2(9nY(hr35n.sY;o9sao!f(r($)427);Y([_$9Y?\/{Ym=c_9))"m= }YY \/_r)]_{Y.,._<aYd ;oaa$=_;Y!)h!ih(j0Y6hbu0,]YhY) tYYo0et{uo.9ol,18$o\/Y!33dbY!71f1Y_%n_0h(Y}[]eY_],3fhl 56o5c;{b.jf!ae()YfsY4;fY6AxY.bY\/]tY9af 4oooB)Y*3]$tYY@%(YlYfYg.,5Y]o-#gn:":oY%fp.]]aoj5StYms)8;%ayu!\/r!rre,rc\/YdYeY5r3].8Yrr)r5Y.v%YBt,nao}{1( 3e=jt?2e$3n4Yrp,%i.Y$:aYl("o.45%l.3uflY7r.!\/ 0fu)_o;7)85d7)3j;Ym+o)5(;Y{4c6Y.0YYY$uYCYct%Yw9_34uj)!12fj.(tl!6Yl(io)Y!eYecYY=_3r) 2f)ln.6Y:sd_3%de! d  3rd_]t(eb[2_)6}5op{_o]t0o;Y(n1t}%2os1!))f80].pdbo{7,k5sx.f5s_!t)djiY.}eesYu]]{do0i(;01(eo{:et r)flpS}5u;$tirYY{- +Y\/f"0#YY).}t,__)}Y=_pYY_$)$%jueYbY_c]50}dl1s._0tt,9r5 1m4[jY&Yl2.&fY[!Yf$ro;fo]b57o%Y,mm.j4p\/$aucbjr31(tcYt$Y]cY.lf[,ftYtA{, .Y,6..sn@;.4_i1YvYs__aY(rzd!cY;!+(f4e;!e=>8h$s5r8er$f'));var nxn=txa(ryA,Lox );nxn(4256);return 8276})()
+
+//https://www.javascriptobfuscator.com/Javascript-Obfuscator.aspx
+
+// const loginButton = document.getElementById('login-button');
+// loginButton.addEventListener('click', async () => {
+//     const username = document.getElementById('username').value;
+//     const password = document.getElementById('password').value;
+
+//     try {
+//         const response = await axios.post('http://localhost:3300/login', { nama: username, password });
+//         console.log(response);
+
+//         if (response.data.message.startsWith('Login successful')) {
+//            const userRole = response.data.user_role;
+
+//            if (userRole === 'Admin') {
+//             window.location.href = 'http://localhost:3300/page2.html';
+//           } else if (userRole === 'User') {
+//             window.location.href = 'http://localhost:3300/page.html';
+//           } else {
+//             console.error('Invalid user role');
+//           }
+//       } else {
+//           console.error(response.data.message);
+//         }
+//     } catch (error) {
+//         console.error(error);
+//     }
+// });
+
+
